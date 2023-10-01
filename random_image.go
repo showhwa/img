f1108232bb523204e82f12ecc3ca9ec9861df2c6
showhwa <bqmygl@gmail.com>
2023-10-01 22:46:12 +0800

    update code

--- random_image.go
+++ random_image.go
@@ -8,16 +8,32 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
-var imageNames []string
-var baseImageUrl string
-var textFileURL string
+const (
+	defaultDownloadTime = 5
+	serverPort          = 8088
+)
+
+var (
+	imageNames   []string
+	baseImageUrl string
+	textFileURL  string
+	log          = logrus.New()
+)
+
+func init() {
+	log.SetFormatter(&logrus.TextFormatter{})
+	log.SetLevel(logrus.InfoLevel)
+}
 
 func downloadTextFile(url string) error {
 	response, err := http.Get(url)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to download text file: %v", err)
 	}
 	defer response.Body.Close()
 
@@ -29,14 +45,13 @@ func downloadTextFile(url string) error {
 	}
 
 	if err := scanner.Err(); err != nil {
-		return err
+		return fmt.Errorf("failed to read text file: %v", err)
 	}
 
 	return nil
 }
 
 func getRandomImageURL() string {
-	rand.Seed(time.Now().UnixNano())
 	randomImageName := imageNames[rand.Intn(len(imageNames))]
 	return fmt.Sprintf("%s/%s", baseImageUrl, randomImageName)
 }
@@ -48,19 +63,22 @@ func getRandomImageURLHandler(w http.ResponseWriter, r *http.Request) {
 
 func startDownloadTimer(interval time.Duration) {
 	ticker := time.NewTicker(interval)
-
 	for range ticker.C {
-		err := downloadTextFile(textFileURL)
-		if err != nil {
-			fmt.Println("Error downloading the text file:", err)
+		if err := downloadTextFile(textFileURL); err != nil {
+			log.Errorf("Error downloading the text file: %v", err)
 		} else {
-			fmt.Println("Downloaded the text file at", time.Now())
+			log.Info("Downloaded the text file at", time.Now())
 		}
 	}
 }
 
-func main() {
+func setupRoutes() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", getRandomImageURLHandler)
+	return router
+}
 
+func main() {
 	textFileURL = os.Getenv("FILE_URL")
 	USER := os.Getenv("USER")
 	REPO := os.Getenv("REPO")
@@ -68,8 +86,8 @@ func main() {
 
 	downloadTimeStr := os.Getenv("downloadTime")
 	downloadTime, err := strconv.Atoi(downloadTimeStr)
-	if err != nil {
-		downloadTime = 5
+	if err != nil || downloadTime <= 0 {
+		downloadTime = defaultDownloadTime
 	}
 
 	baseImageUrl = fmt.Sprintf("https://cdn.jsdelivr.net/gh/%s/%s/%s", USER, REPO, FILE_PATH)
@@ -78,13 +96,12 @@ func main() {
 
 	err = downloadTextFile(textFileURL)
 	if err != nil {
-		fmt.Println("Error downloading the text file:", err)
+		log.Errorf("Error downloading the text file: %v", err)
 		return
 	}
 
-	router := http.NewServeMux()
-	router.HandleFunc("/", getRandomImageURLHandler)
-	fmt.Printf("FILE_URL = %s, USER = %s, REPO = %s, FILE_PATH = %s, downloadTime = %d \n", textFileURL, USER, REPO, FILE_PATH, downloadTime)
-	fmt.Println("Server started at :8088")
-	http.ListenAndServe(":8088", router)
+	router := setupRoutes()
+	log.Infof("FILE_URL = %s, USER = %s, REPO = %s, FILE_PATH = %s, downloadTime = %d", textFileURL, USER, REPO, FILE_PATH, downloadTime)
+	log.Infof("Server started at :%d", serverPort)
+	http.ListenAndServe(fmt.Sprintf(":%d", serverPort), router)
 }
